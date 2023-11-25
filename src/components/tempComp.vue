37470dcf5211e16bbaa54df46e2dc4ce755dbe96
cocohyuncoco <guswjd8287@gmail.com>
2023-11-25 20:10:25 +0900

    [임현정] 자동 라우팅 등록에서 components 폴더는 제외시키도록 설정

---
+++ src/components/tempComp.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-h4">
+        Temp Comp
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
