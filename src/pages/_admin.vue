3f252c4b950be0eaa537fbebb545a46f75ae3dfd
cocohyuncoco <guswjd8287@gmail.com>
2023-11-26 20:14:53 +0900

    [임현정] admin layout으로 이동시 ROOT 파일에 router-view 사용
    상세 내용 :
    - vite 라이브러리 사용시 레이아웃 변환 과정에서 admin/dashboard 이면 플러그인이 router를 못하는 에러가 있다
    - 해결 방법으로 admin 폴더와 똑같은 admin.vue 파일을 만들고 그 안에 router-view 태그를 사용한것

---
+++ src/pages/_admin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <router-view />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+<route lang="yaml">
+meat :
+    layout : admin
+</route>
---
+++ src/pages/admin/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="text-h4">
+            admin/index
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
